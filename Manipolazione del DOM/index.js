5cde87218e74eba8a7ff463e21ad1b1c86c68712
mikedarmand <mikedarmand@users.noreply.github.com>
2024-07-04 10:48:52 +0200

    list created

---
+++ Manipolazione del DOM/index.js
@@ -0,0 +1,9 @@
+
+
+
+const addProduct = () => {
+  const mylist = document.createElement('myList')
+  mylist.innerText('siamo allafine di questo lungo percorso')
+  const checkbox = document.createElement('checkbox')
+  console.log(checkbox)
+};
